e8a208ca1b82a288faa51b6f2d4db6df8b3e266c
Clarence <c.goh@digipen.edu>
2023-06-20 16:27:19 +0800

    Trash Bin Day 1 & 30 + Particles

--- Assets/Scripts/RecyclingBin.cs
+++ Assets/Scripts/RecyclingBin.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class RecyclingBin : MonoBehaviour
 {
-    // Anyone know a better way to do this lol
-    public static Action recyclingAction;
-
+    [SerializeField]
+    private int trashCapacity = 3;
+    [SerializeField]
+    private int curCapacity = 0;
+    public GameObject[] trashes;
+    public GameObject fireSmoke;
     // Start is called before the first frame update
     void Start()
     {
-        recyclingAction = transform.parent.GetComponent<Action>();
+        trashes = new GameObject[trashCapacity];
+        for (int i = 1; i <= trashCapacity; ++i)
+        {
+            trashes[i-1] = transform.GetChild(i).gameObject;
+        }
     }
 
     // Update is called once per frame
@@ -22,15 +29,17 @@ public class RecyclingBin : MonoBehaviour
     // Check if trash is thrown into the right bin
     void OnTriggerEnter(Collider col)
     {
-        if(col.CompareTag(gameObject.tag))
+        if (col.CompareTag(gameObject.tag))
         {
-            col.gameObject.SetActive(false);
-            recyclingAction.GoodOutcome();
+            if (curCapacity < trashCapacity)
+            {
+                //GameObject tempSmoke = Instantiate(fireSmoke, transform.position, Quaternion.Euler(0,0,0));
+                col.gameObject.SetActive(false);
+                trashes[curCapacity++].SetActive(true);
+                if (curCapacity == trashCapacity) {
+                    // Check Task List
+                }
+            }
         }
-        else
-        {
-            col.gameObject.SetActive(false);
-            recyclingAction.BadOutcome();
-        } 
     }
 }
