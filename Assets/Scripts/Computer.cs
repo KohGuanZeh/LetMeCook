c38b7d7d862a8d9ec9cc72557159f546ea8bd494
Nigel Koh <kohyuanqi@gmail.com>
2023-06-21 15:49:34 +0800

    Edited aircon script

--- Assets/Scripts/Computer.cs
+++ Assets/Scripts/Computer.cs
@@ -3,5 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Computer : MonoBehaviour {
+    [SerializeField] AirCondition aircon;
 
+    public void doWork() {
+        if (aircon.on) {
+            GameManager.inst.OnWorkDone();
+        }
+    }
 }
--- Assets/Scripts/TempController.cs
+++ Assets/Scripts/TempController.cs
@@ -3,23 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TempController : MonoBehaviour {
-    public int roomTemp;
-    public int globalTemp;
     public AirCondition airCon;
 
     void Start() {
-        roomTemp = 32;
-        globalTemp = 37;
         airCon = FindObjectOfType<AirCondition>();
     }
-    
-    void Update() {
-        ChangeTemp();
-    }
 
-    void ChangeTemp() {
-        if (airCon.on) {
-            roomTemp = airCon.temp;
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            airCon.on = true;
         }
     }
 }
